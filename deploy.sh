f051d928aca7e8dec6c597dfb920d176347fa5e9
Olli Janatuinen <olli.janatuinen@gmail.com>
2022-04-12 16:07:26 +0200

    Switch to RSA 4096 because Azure DevOps does not support Ed25519

--- deploy.sh
+++ deploy.sh
@@ -17,15 +17,15 @@ if [ $? == 0 ]; then
     exit 1
 fi
 
-ssh-keygen -t ed25519 -f id_ed25519 -q -N ""
+ssh-keygen -t rsa -b 4096 -f id_rsa -q -N ""
 
 kubectl apply -f base/gitops-agent/namespace.yaml
-kubectl -n k8s4u-gitops-agent create secret generic k8s4u-gitops-agent-git-ssh-key --from-file=id_ed25519 --from-file=id_ed25519.pub
-rm id_ed25519
+kubectl -n k8s4u-gitops-agent create secret generic k8s4u-gitops-agent-git-ssh-key --from-file=id_rsa --from-file=id_rsa.pub
+rm id_rsa
 
 kubectl apply -k envs/$1
 
 echo
 echo "Please, import following SSH public key to your Git environment:"
-cat id_ed25519.pub
-rm id_ed25519.pub
+cat id_rsa.pub
+rm id_rsa.pub
